9f758316d4d6fe428751a41e7bfb695b652ae325
Zack Siri <zack@codemy.net>
2015-04-19 22:48:42 +0700

    clean up movie partial and layouts

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ActiveRecord::Base
 	has_and_belongs_to_many :movies
+
+  validates :name, uniqueness: true
 end
--- db/migrate/20150419152045_create_genres_movies.rb
+++ db/migrate/20150419152045_create_genres_movies.rb
@@ -1,8 +1,8 @@
 class CreateGenresMovies < ActiveRecord::Migration
   def change
     create_table :genres_movies, id: false do |t|
-    	t.integer :genre_id, index: true
-    	t.integer :movie_id, index: true
+      t.integer :genre_id, index: true, unique: true
+      t.integer :movie_id, index: true
     end
   end
 end
