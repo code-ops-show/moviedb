2fd7929ec5b7e3dfdf7f1551121aa9a10e902e5d
Zack Siri <zack@codemy.net>
2015-05-01 15:43:00 +0700

    added test for movie to raise exception

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,13 +7,22 @@ class Movie < ActiveRecord::Base
 	has_and_belongs_to_many :genres
   has_and_belongs_to_many :crews
 
+  class RelationError < StandardError
+    def initialize(msg = "That Relationship Type doesn't exist")
+      super(msg)
+    end
+  end
+
   def add_many(type, data)
-    type.in? ['Genre', 'Crew']
-    type_plural = type.downcase.pluralize
-    
-    self.send("#{type_plural}=", data.map do |g|
-      type.classify.constantize.where(name: g).first_or_create!
-    end)
+    if type.in? ['Genre', 'Crew']
+      type_plural = type.downcase.pluralize
+
+      self.send("#{type_plural}=", data.map do |g|
+        type.classify.constantize.where(name: g).first_or_create!
+      end)
+    else
+      raise RelationError
+    end
   end
 
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,23 +1,30 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  test "add genres" do 
-    movie = movies(:avengers)
 
+  def setup 
+    @movie = movies(:avengers)
+  end
+
+  test "add genres" do 
     assert_difference('Genre.count', 1) do 
-      movie.add_many 'Genre', ['Comedy', 'Action']
+      @movie.add_many 'Genre', ['Comedy', 'Action']
     end
 
-    assert_equal 2, movie.genres.count
+    assert_equal 2, @movie.genres.count
   end 
 
-  test "add crews" do 
-    movie = movies(:avengers)
-    
+  test "add crews" do     
     assert_difference('Crew.count', 1) do 
-      movie.add_many 'Crew', ['Scarlett Johansson', 'Mark Ruffalo']
+      @movie.add_many 'Crew', ['Scarlett Johansson', 'Mark Ruffalo']
     end
 
-    assert_equal 2, movie.crews.count
+    assert_equal 2, @movie.crews.count
   end
+
+  test "add wrong type" do 
+    assert_raises Movie::RelationError do 
+      @movie.add_many 'Blah', ['Stuff', 'Another Stuff']
+    end
+  end 
 end
