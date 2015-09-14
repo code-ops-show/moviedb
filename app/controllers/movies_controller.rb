44922a3057f7616c1b9487e069ba9e05f23cea4d
Zack Siri <zack@codemy.net>
2015-09-14 23:43:31 +0700

    beautify search url

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
+  before_filter :beautify_search_url, only: [:index]
+
   def index
-    @movies = Movie.custom_search((params[:q].present? ? params[:q] : '*')).records
+    @movies = Movie.custom_search((params[:query].present? ? params[:query] : '*')).records
+  end
+
+private
+
+  def beautify_search_url
+    redirect_to search_movies_path(query: params[:q]) if params[:q].present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
+  resources :movies, only: [:show, :index] do 
+    get 'search/*query', to: 'movies#index', as: :search, on: :collection
+  end
+
   resource :landing, only: [:show]
 
   root to: 'landings#show'
