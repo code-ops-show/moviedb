d7b2e79b8e7c9a278e52c9e8f9335308207128b5
Zack Siri <zack@codemy.net>
2015-10-23 14:17:34 +0700

    fix use refiner gem

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,10 @@
 class MoviesController < ApplicationController
+  include Refiner::Query
+  
   before_filter :beautify_search_url, only: [:index]
 
   def index
-    search  = Movie.custom_search(segmented_query)
+    search  = Movie.custom_search(segment_refiner_query_by("crews", "keyword", "genres"))
     @aggs   = search.response.aggregations.map { |a| AggregationPresenter.new(a) }
     @movies = search.results
   end
@@ -12,16 +14,4 @@ private
   def beautify_search_url
     redirect_to search_movies_path(query: "keyword/#{params[:q]}") if params[:q].present?
   end
-
-  def segmented_query
-    query.slice(*["crews", "keyword", "genres"]) rescue {}
-  end
-
-  helper_method :segmented_query
-
-  def query
-    Hash[*params[:query].split(/\//)] rescue {}
-  end
-
-  helper_method :query
 end
